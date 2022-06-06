747100f211161a392252cc5e955e24bec7d39532
sara2993 <saraswathi.mani@chainsys.com>
2022-06-06 22:52:18 +0530

    commit

--- src/server/database.js
+++ src/server/database.js
@@ -1,12 +1,12 @@
 const { ɵCompiler_compileModuleAndAllComponentsAsync__POST_R3__ } = require("@angular/core");
 const Cloudant = require("@cloudant/cloudant");
-var url =
+let url =
   "https://a7e75d33-40d2-47a6-a9b9-f80dbbc41c98-bluemix.cloudantnosqldb.appdomain.cloud/";
-var username = "apikey-v2-ijzqz68xo4ar5nrlcenfueq1cy3mgg675nzk8td8x9w";
-var password = "e455d34a303110b468819fbc14388b5e";
+let username = "apikey-v2-ijzqz68xo4ar5nrlcenfueq1cy3mgg675nzk8td8x9w";
+let password = "e455d34a303110b468819fbc14388b5e";
  
-var cloudant = Cloudant({ url: url, username: username, password: password });
-var foodchain=cloudant.use('textile_industry');
+let cloudant = Cloudant({ url: url, username: username, password: password });
+let foodchain=cloudant.use('textile_industry');
 data={
   selector:{
     id:'user',
