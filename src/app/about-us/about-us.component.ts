67c4c78ee626dd3b1f229bcd7370af948214b5fe
sara2993 <saraswathi.mani@chainsys.com>
2022-06-06 20:23:37 +0530

    commit

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("constructor");
+   }
 
   ngOnInit(): void {
+    console.log("ngOnInit");
   }
+
 
 }
